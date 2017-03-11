e2f264411f1c7ee52f093836007c042faf7bf09d
Mnilko <mezentsev.valentin@gmail.com>
2017-03-12 01:34:15 +0200

    Daily functions

--- app/daily_intent.py
+++ app/daily_intent.py
@@ -31,7 +31,29 @@ def add_nutriotion(nutrition, value):
     else:
         user = User.query.get(session.attributes['user_id'])
         daily = Daily.query.filter(Daily.users.contains(user) & (Daily.created_timestamp >= datetime.date.today())).first()
-        setattr(daily, nutrition, int(value))
+        sum_value = daily.__getattribute__(nutrition) + int(value)
+        setattr(daily, nutrition, sum_value)
         db.session.commit()
         response_msg = "You daily summary: %i proteins, %i carbs, %i fats." % (daily.protein, daily.carb, daily.fat)
         return question(response_msg)
+
+@ask.intent('DailyLeftNutritionIntent')
+def left_nutriotion():
+    if ('user_id' not in session.attributes):
+        return question("Can't find without user. What is your name?")
+    else:
+        user = User.query.get(session.attributes['user_id'])
+        daily = Daily.query.filter(Daily.users.contains(user) & (Daily.created_timestamp >= datetime.date.today())).first()
+        if daily is None:
+            daily = Daily()
+            user.dailies.append(daily)
+            db.session.add(daily)
+            db.session.commit()
+            response_msg = "You didn't add any food today. Do you want to add something?"
+            return question(response_msg)
+        else:
+            protein_left = user.protein - daily.protein
+            carb_left = user.carb - daily.carb
+            fat_left = user.fat - daily.fat
+            response_msg = "You need to eat: %i protein, %i carbs, %i fat." % (protein_left, carb_left, fat_left)
+            return question(response_msg)

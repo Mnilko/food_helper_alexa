5dea4c3908cf1167a81b179453fa4c491d015a0c
Mnilko <mezentsev.valentin@gmail.com>
2017-03-12 00:10:00 +0200

    Refatoring

--- app/daily_intent.py
+++ app/daily_intent.py
@@ -4,7 +4,7 @@ from models import *
 daily_intent = Blueprint('daily_intent', __name__)
 
 @ask.intent('CurrentDailyIntent')
-def current_intent(self, arg):
+def current_intent():
     if 'user_id' not in session.attributes:
         return question("Can't find without user. What is your name?")
     else:
@@ -26,9 +26,12 @@ def current_intent(self, arg):
 def add_nutriotion(nutrition, value):
     if ('user_id' not in session.attributes):
         return question("Can't find without user. What is your name?")
-    elif ('nutrition' is None) | ('value' is None):
-
+    elif (nutrition is None) | (value is None):
+        return question("Please, provide nutriotion and amout.")
     else:
         user = User.query.get(session.attributes['user_id'])
         daily = Daily.query.filter(Daily.users.contains(user) & (Daily.created_timestamp >= datetime.date.today())).first()
-        setattr(daily, , 10)
+        setattr(daily, nutrition, int(value))
+        db.session.commit()
+        response_msg = "You daily summary: %i proteins, %i carbs, %i fats." % (daily.protein, daily.carb, daily.fat)
+        return question(response_msg)
--- app/nutriotion_list_intent.py
+++ app/nutriotion_list_intent.py
@@ -3,7 +3,7 @@ from models import *
 
 nutriotion_list_intent = Blueprint('nutriotion_list_intent', __name__)
 
-@ask.intent("MyNutrionIntent")
+@ask.intent("MyNutritionIntent")
 def get_nutriotion_list():
     if 'user_id' not in session.attributes:
         return question("Can't find without user. What is your name?")
@@ -12,35 +12,15 @@ def get_nutriotion_list():
         user_info_list = "Your nutrion list is: proteins %i, carbs %i, fats %i." % (user.protein, user.carb, user.fat)
         return question(user_info_list)
 
-@ask.intent("ChangeProteinIntent", mapping={'value': 'Value'})
-def change_protein_value(value):
-    if 'user_id' not in session.attributes:
-        return question("Can't change without user. What is your name?")
+@ask.intent("ChangeNutritionList", mapping={'nutrition': 'Nutrition', 'value': 'Value'})
+def change_nutrition_value(nutrition, value):
+    if ('user_id' not in session.attributes):
+        return question("Can't find without user. What is your name?")
+    elif (nutrition is None) | (value is None):
+        return question("Please, provide nutriotion and amout.")
     else:
         user = User.query.get(session.attributes['user_id'])
-        user.protein = int(value)
+        setattr(user, nutrition, int(value))
         db.session.commit()
-        result_msg = "Your protein goal set to %s grams per day." % value
-        return question(result_msg)
-
-@ask.intent("ChangeCarbIntent", mapping={'value': 'Value'})
-def change_carb_value(value):
-    if 'user_id' not in session.attributes:
-        return question("Can't change without user. What is your name?")
-    else:
-        user = User.query.get(session.attributes['user_id'])
-        user.carb = int(value)
-        db.session.commit()
-        result_msg = "Your carb goal set to %s grams per day." % value
-        return question(result_msg)
-
-@ask.intent("ChangeFatIntent", mapping={'value': 'Value'})
-def change_fat_value(value):
-    if 'user_id' not in session.attributes:
-        return question("Can't change without user. What is your name?")
-    else:
-        user = User.query.get(session.attributes['user_id'])
-        user.fat = int(value)
-        db.session.commit()
-        result_msg = "Your fat goal set to %s grams per day." % value
-        return question(result_msg)
+        response_msg = "%s goal seted to %s" % (nutrition, user.__getattribute__(nutrition))
+        return question(response_msg)
